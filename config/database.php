56ec31558e7412ab5e85b792d297a742b3217c04
coco <15940926841@163.com>
2018-12-10 16:26:25 +0800

    init

--- config/database.php
+++ config/database.php
@@ -19,11 +19,11 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '106.12.15.151',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'antDB',
     // 用户名
-    'username'        => 'back',
+    'username'        => 'root',
     // 密码
     'password'        => '123',
     // 端口

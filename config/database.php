38171b74559392c39462909d78082f5ba80c1973
coco <15940926841@163.com>
2018-12-17 17:54:04 +0800

    database

--- config/database.php
+++ config/database.php
@@ -15,18 +15,18 @@
 //ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Root1.^12222'
 //ALTER USER 'root'@'localhost' IDENTIFIED BY '123';
 //GRANT ALL PRIVILEGES ON *.* TO 'kgc'@'%' IDENTIFIED BY '123' WITH GRANT OPTION;
-
+// antqueen  Root1.^12222
 return [
    // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
      'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'antqueen',
+    'database'        => 'antDB',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'Root1.^12222',
+    'password'        => '123',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
